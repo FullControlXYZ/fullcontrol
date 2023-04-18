00c24af984d92e365161d4e800c4510f60070d1d
andy g <andy.gleadall@gmail.com>
2023-04-18 13:28:07 +0100

    added offset_path function and docs

--- lab/fullcontrol/geometry/__init__.py
+++ lab/fullcontrol/geometry/__init__.py
@@ -6,3 +6,4 @@ from lab.fullcontrol.geometry.bezier import bezierXYdiscrete
 from lab.fullcontrol.geometry.convex import convex_pathsXY
 from lab.fullcontrol.geometry.reflectXYpolar_list import reflectXYpolar_list
 from lab.fullcontrol.geometry.intersect import line_intersection_by_points_XY, line_intersection_by_polar_XY, crossing_lines_check_XY
+from lab.fullcontrol.geometry.offset_path import offset_path
