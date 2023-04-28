6a9b220af8a18e72edeb911aa60efa636ed347e4
andy g <andy.gleadall@gmail.com>
2023-04-28 14:43:15 +0100

    modified new demo models

--- bin/colab_models.py
+++ bin/colab_models.py
@@ -13,9 +13,12 @@ notebook_addresses = ["../models/" + notebook_name for notebook_name in notebook
 
 old_import = "import fullcontrol as fc"
 new_import = "if 'google.colab' in str(get_ipython()):\\n  !pip install git+https://github.com/FullControlXYZ/fullcontrol --quiet\\n" + old_import
+old_gcode = "gcode = fc.transform(steps, 'gcode', gcode_controls)"
+new_gcode = old_gcode + "\\nopen(f'{design_name}.gcode', 'w').write(gcode)\\nfiles.download(f'{design_name}.gcode')"
 
 for notebook_address in notebook_addresses:
     content_string = open(notebook_address).read()
     # replace import statement with install+import statements:
     content_string = content_string.replace(old_import, new_import)
+    content_string = content_string.replace(old_gcode, new_gcode)
     open(f'{notebook_address[:-6].replace("models","models/colab")}_colab.ipynb', 'w').write(content_string)
