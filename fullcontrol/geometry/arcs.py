9fcb1ad46fc22f8a02d4580ada169671df9ff895
Hannah Eikens <hannah@eikens.com>
2023-06-28 13:06:07 +0200

    Added arcXY

--- fullcontrol/geometry/arcs.py
+++ fullcontrol/geometry/arcs.py
@@ -1,7 +1,7 @@
 
 from fullcontrol.common import linspace
 from fullcontrol.geometry import Point, polar_to_point, ramp_xyz, ramp_polar
-from math import tau
+from math import tau, sin , cos
 
 
 def arcXY(centre: Point, radius: float, start_angle: float, arc_angle: float, segments: int = 100) -> list:
@@ -31,3 +31,12 @@ def arcXY(centre: Point, radius: float, start_angle: float, arc_angle: float, se
     '''
     a_steps = linspace(start_angle, start_angle+arc_angle, segments+1)
     return [polar_to_point(centre, radius, a) for a in a_steps]
+
+def elliptical_arcXY(centre: Point, a: float, b: float, start_angle: float, arc_angle: float, segments: int=100) -> list:
+    '''generate a 2d-xy elliptical arc with the specified number of segments, centred about a point, 
+    for the given a (x-width) and b (y-height), given the arc angle (radians), starting at the specified polar angle (radians), 
+    with z-position the same as that of the centre point. return list of Points
+    '''
+    
+    t_steps = linspace(start_angle, start_angle+arc_angle, segments+1)
+    return [Point(x=a*cos(t) + centre.x, y=b*sin(t) + centre.y, z=centre.z) for t in t_steps]
