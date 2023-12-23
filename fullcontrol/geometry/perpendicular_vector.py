20db71d4030b43b47806c6c20e68fcfcb45446c8
Hannah Eikens <hannah@eikens.com>
2023-12-23 17:57:32 +0100

    Updated naming and descriptions for perpendicular_vector.py

--- fullcontrol/geometry/perpendicular_vector.py
+++ fullcontrol/geometry/perpendicular_vector.py
@@ -1,11 +1,11 @@
 from fullcontrol.geometry import Point, Vector
 
-def perpendicular_vectorXY(point1: Point, point2: Point) -> Vector:
-    'returns perpendicular 2d-vector from a line'
+def perpendicular_vectorXY(start_point: Point, end_point: Point) -> Vector:
+    'returns perpendicular 2d-vector from a line through two points, according to the right-hand rule'
 
     return Vector(x=-(point2.y-point1.y), y=point2.x-point1.x)
 
 def perpendicular_vectorXY_from_vector(vector: Vector) -> Vector:
-    'returns perpendicular 2d-vector to a vector'
+    'returns perpendicular 2d-vector to a vector, according to the right-hand rule'
     
     return Vector(x=-vector.y, y=vector.x)
