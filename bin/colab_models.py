65c9d62fa1df6d7be14915304eb906eef2bdf375
andy g <fullcontrolxyz@gmail.com>
2024-02-28 09:17:02 +0000

    added phone stand model

--- bin/colab_models.py
+++ bin/colab_models.py
@@ -10,10 +10,10 @@ notebook_names = ["design_template.ipynb",
                   "fractional_design_engine_polar.ipynb",
                   "ripple_texture.ipynb",
                   "nuts_and_bolts.ipynb",
-                  "blob_printing.ipynb"]
+                  "blob_printing.ipynb",
+                  "anyangle_phone_stand.ipynb"]
 
-notebook_addresses = ["../models/" +
-                      notebook_name for notebook_name in notebook_names]
+notebook_addresses = ["../models/" + notebook_name for notebook_name in notebook_names]
 
 old_intro = "*this document is a jupyter notebook - if they're new to you, check out how they work: [link](https://www.google.com/search?q=ipynb+tutorial), [link](https://jupyter.org/try-jupyter/retro/notebooks/?path=notebooks/Intro.ipynb), [link](https://colab.research.google.com/)*"
 new_intro = "*this document is a jupyter notebook - if they're new to you, check out how they work: [link](https://www.google.com/search?q=ipynb+tutorial), [link](https://jupyter.org/try-jupyter/retro/notebooks/?path=notebooks/Intro.ipynb), [link](https://colab.research.google.com/)*\\n### be patient :)\\n\\nthe next code cell may take a while because running it causes several things to happen:\\n- connect to a google colab server -> download the fullcontrol code -> install the fullcontrol code\\n\\ncheck out [other tutorials](https://github.com/FullControlXYZ/fullcontrol/blob/master/docs/README.md) to understand the python code for the FullControl design"
@@ -23,8 +23,7 @@ old_import = "import fullcontrol as fc"
 new_import = "if 'google.colab' in str(get_ipython()):\\n  !pip install --no-deps git+https://github.com/FullControlXYZ/fullcontrol --quiet\\n  # --no-deps is included due to a pip 'dependency resolver' error in colab that began in Feb 2024\\n" + \
     old_import + "\\nfrom google.colab import files"
 old_gcode = "gcode = fc.transform(steps, 'gcode', gcode_controls)"
-new_gcode = old_gcode + \
-    "\\nopen(f'{design_name}.gcode', 'w').write(gcode)\\nfiles.download(f'{design_name}.gcode')"
+new_gcode = old_gcode + "\\nopen(f'{design_name}.gcode', 'w').write(gcode)\\nfiles.download(f'{design_name}.gcode')"
 old_gcode2 = "    save_as=design_name,"
 new_gcode2 = ""
 
@@ -35,7 +34,11 @@ for notebook_address in notebook_addresses:
     content_string = content_string.replace(old_intro_runall, new_intro_runall)
     content_string = content_string.replace(old_intro, new_intro)
     content_string = content_string.replace(old_import, new_import)
-    content_string = content_string.replace(old_gcode, new_gcode)
+    # the next if statement accomodates notebooks that use "if target == 'gcode':"
+    if "    gcode = fc.transform(steps, 'gcode', gcode_controls)" in content_string:
+        content_string = content_string.replace(            old_gcode, new_gcode.replace("\\n", "\\n    "))
+    else:
+        content_string = content_string.replace(old_gcode, new_gcode)
     content_string = content_string.replace(old_gcode2, new_gcode2)
     open(f'{notebook_address[:-6].replace("models","models/colab")}_colab.ipynb',
          'w').write(content_string)
