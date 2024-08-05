26b92e11e760fe3c8dbde99b4fec686911bbba77
andy g <fullcontrolxyz@gmail.com>
2024-08-05 18:18:34 +0100

    removed return statement for stl generation for clarity since the stl nor any data is returned

--- lab/fullcontrol/transform.py
+++ lab/fullcontrol/transform.py
@@ -7,5 +7,5 @@ def transform(steps: list, result_type: str, controls: ModelControls = None):
     if result_type == '3d_model':  # This is currently redundant, but maintained for consistency with fullcontrol.combinations.gcode_and_visualization.common in any future expansion to the result_type options here
         from lab.fullcontrol.geometry_model.steps2geometry import geometry_model
         if controls is not None:
-            return geometry_model(steps, controls)
-        return geometry_model(steps)
+            geometry_model(steps, controls)
+        geometry_model(steps)
